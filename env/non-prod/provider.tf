8ef2bd4346a1a08d866db04c84988177057c8706
Ankur Srivastava <ankur.ds.aws@gmail.com>
2025-06-18 16:39:45 +0530

    My First Commit

--- env/non-prod/provider.tf
+++ env/non-prod/provider.tf
@@ -15,23 +15,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-# variable "aws_region" {
-#   type        = string
-#   default     = "ap-south-1"
-#   description = "AWS Region"
-# }
-
-
-# terraform {
-#   backend "s3" {
-#     bucket         = "darpg-tfstate-bucket"            # ✅ Replace with your bucket
-#     key            = "env/dev/terraform.tfstate"       # ✅ tfstate path inside bucket
-#     region         = "ap-south-1"                       # ✅ your bucket region
-#     encrypt        = true
-#     dynamodb_table = "terraform-locks"                 # ✅ optional, for locking
-#   }
-# }
-
-# provider "aws" {
-#   region = var.aws_region
-# }
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
